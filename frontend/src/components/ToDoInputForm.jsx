1ca97d8e76b8ac9877119034a69ef4571ef83c61
Mike MacDougall <mgmacdougall@outlook.com>
2025-10-25 18:52:08 -0400

    Add of react bootstrap styling to input fields

--- frontend/src/components/ToDoInputForm.jsx
+++ frontend/src/components/ToDoInputForm.jsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./ToDoInputForm.css";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 function ToDoInputForm({ handleToDoSubmit }) {
   const [title, setTitle] = useState("");
   const [completed, setCompleted] = useState(false);
@@ -11,29 +16,43 @@ function ToDoInputForm({ handleToDoSubmit }) {
   };
 
   return (
-    <div>
+    <div className="form-container">
       <h2>Enter a new todo</h2>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="border border-gray-300 rounded p-2 w-full"
-        />
-        <label htmlFor="complete">Completed</label>
-        <select
-          id="complete"
-          value={completed}
-          onChange={(e) => setCompleted(e.target.value === "true")}
-          className="border border-gray-300 rounded p-2 w-full"
-        >
-          <option value={false}>False</option>
-          <option value={true}>True</option>
-        </select>
-        <button type="submit">Save To Do</button>
-      </form>
+      <Form onSubmit={handleFormSubmit}>
+        <Row>
+          <label htmlFor="title">Title:</label>
+          <Col>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="border border-gray-300 rounded p-2 w-full"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <label htmlFor="complete">Completed</label>
+          <Col>
+            <select
+              id="complete"
+              value={completed}
+              onChange={(e) => setCompleted(e.target.value === "true")}
+              className="border border-gray-300 rounded p-2 w-full"
+            >
+              <option value={false}>False</option>
+              <option value={true}>True</option>
+            </select>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col className="d-flex justify-content-end" size="sm">
+            <Button id="form-submit" type="submit">
+              Save To Do
+            </Button>
+          </Col>
+        </Row>
+      </Form>
     </div>
   );
 }
