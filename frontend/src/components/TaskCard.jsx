bb2da66f6470ae52c405560efdc9eaeda2f3e455
Mike MacDougall <mgmacdougall@outlook.com>
2025-10-25 18:50:28 -0400

    Add of styling for Task cards

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import "./TaskCard.css";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 function TaskCard({ task, onUpdate, onDelete }) {
   const [isEdit, setIsEdit] = useState(false);
@@ -15,37 +17,34 @@ function TaskCard({ task, onUpdate, onDelete }) {
       <h2>{task.title}</h2>
       {isEdit ? (
         <div>
-          <p>Edit state</p>
+          {/* <p>Edit state</p> */}
           <input
             type="text"
             value={taskTitle}
             onChange={(e) => setTitle(e.target.value)}
             className="w-full mb-2 p-2 border rounded"
           />
-          {/* <input
-            type="text"
-            value={task.completed}
-            className="w-full mb-2 p-2 border rounded"
-          /> */}
-
-          <button
+          <Button
+            variant="primary"
             onClick={() => {
               handleEditClick();
               onUpdate(task._id, { title: taskTitle });
             }}
           >
             Save changes
-          </button>
+          </Button>
         </div>
       ) : (
-        <div className="p-4">
-          <p>None edit state</p>
-        </div>
+        <div className="p-4">{/* <p>None edit state</p> */}</div>
       )}
-      <div className="button-group">
-        <button onClick={handleEditClick}>Edit</button>
-        <button onClick={() => onDelete(task._id)}>Delete</button>
-      </div>
+      <ButtonGroup aria-label="Basic example" className="me-2">
+        <Button variant="primary" onClick={handleEditClick}>
+          Edit
+        </Button>
+        <Button variant="danger" onClick={() => onDelete(task._id)}>
+          Delete
+        </Button>
+      </ButtonGroup>
     </div>
   );
 }
