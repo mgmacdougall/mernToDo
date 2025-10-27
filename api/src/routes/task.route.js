009f8222f34fc60a25251b8c1269649d64899def
Mike MacDougall <mgmacdougall@outlook.com>
2025-10-27 10:41:08 -0400

    Adjust of router to use controllers

--- api/src/routes/task.route.js
+++ api/src/routes/task.route.js
@@ -1,65 +1,16 @@
 import express from "express";
 import { Task } from "../models/todo.model.js";
-
+import {
+  getTaskById,
+  createTask,
+  deleteTask,
+  updateTask,
+} from "../controllers/task.controller.js";
+import { create } from "domain";
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
-  try {
-    const foundTask = await Task.find({ _id: req.params.id });
-    if (!foundTask) {
-      return res.status(404).json({ message: "Task not found" });
-    }
-    res.status(200).json({ message: "Task found", task: foundTask });
-  } catch (e) {
-    res.status(500).json({ message: "Error encountered" });
-  }
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const { title, completed } = req.body;
-    console.log(title, completed);
-    const testObj = { title: title, completed: completed, dueDate: new Date() };
-    const newTask = new Task(testObj);
-    const savedTask = await newTask.save();
-    res.status(201).json(savedTask);
-  } catch (e) {
-    res.status(500).json({ error: "Failed to create task" });
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    const deletedTask = await Task.findByIdAndDelete(req.params.id);
-    if (!deletedTask) {
-      return res.status(404).json({ message: "Task not found" });
-    }
-    res
-      .status(200)
-      .json({ message: "Task successfully deleted", task: deletedTask });
-  } catch (e) {
-    res.json({ message: "Error encountered in deleting the task" });
-  }
-});
-
-router.put("/:id", async (req, res) => {
-  try {
-    const { title, completed, dueDate } = req.body;
-    const updatedTask = await Task.findByIdAndUpdate(
-      req.params.id,
-      {
-        title,
-        completed,
-        dueDate,
-      },
-      { new: true, runValidators: true }
-    );
-    if (!updatedTask) {
-      return res.status(404).json({ message: "Task not found" });
-    }
-    res.status(200).json({ message: "Task updated", updatedTask });
-  } catch (e) {
-    res.status(500).json({ message: "Error encountered updating task" });
-  }
-});
+router.get("/:id", getTaskById);
+router.post("/", createTask);
+router.delete("/:id", deleteTask);
+router.put("/:id", updateTask);
 export default router;
